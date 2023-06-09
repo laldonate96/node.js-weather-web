78c6a204e634739cb18aba0484421873459de5bc
laldonate96 <lucasaldonate@gmail.com>
2023-06-09 11:20:44 -0300

    Init commit

---
+++ public/js/app.js
@@ -0,0 +1,26 @@
+console.log('Client side javascript file is loaded!')
+
+const weatherForm = document.querySelector('form') 
+const search = document.querySelector('input') 
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
+weatherForm.addEventListener('submit', (event) => {
+    event.preventDefault()
+
+    const location = search.value // GETS WHATEVER THE USER TYPED INSIDE THE INPUT
+
+    messageOne.textContent = 'Loading...' // SETS WHAT THE PARAGRAPH WILL PRINT
+    messageTwo.textContent = ''
+
+    fetch('http://localhost:3000/weather?adress=' + location).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                messageOne.textContent = data.error
+            } else {
+                messageOne.textContent = data.location
+                messageTwo.textContent = data.forecast
+            }
+        })
+    })
+})
---
+++ src/app.js
@@ -0,0 +1,107 @@
+const path = require('path')
+const express = require('express')
+const hbs = require('hbs')
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
+
+// console.log(path.join(__dirname, '../public')) // CREATES A PATH TO ANOTHER FOLDER
+
+const app = express() // IT IS THE ONLY FUNCTION THE NPM MODULE HAS
+
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+// Setup handlebars engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+// Setup static directory to serve
+app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather',
+        name: 'Lucas Aldonate'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'Lucas Aldonate'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        helpText: 'This is some helpful text.',
+        title: 'Help',
+        name: 'Lucas Aldonate'
+    })
+})
+
+
+app.get('/weather', (req, res) => { // request and response
+    const adress = req.query.adress
+
+    if (!adress) { 
+        return res.send({ // I USE RETURN TO STOP THE EXECUTION, SO THE CODE BELOW THAT PRINTS ON CONSOLE THE SEARCH DOES NOT EXECUTE AND BREAKS THE PROGRAM
+            error: 'You must provide an adress'
+        })
+    } 
+
+    geocode(adress, (error, { latitude, longitude, location } = {}) => {
+        if (error) {
+            return res.send({ error }) //SHORTHAND METHOD WHEN KEY = VALUE
+        } 
+    
+        forecast(latitude, longitude, (error, forecastData) => { 
+            if (error) {
+                return res.send({ error })
+            }
+    
+            res.send({
+                location,
+                forecast: forecastData,
+                adress
+            })
+        })
+    })
+})
+
+
+app.get('/products', (req, res) => {
+    if (!req.query.search) {
+        return res.send({ // I USE RETURN TO STOP THE EXECUTION, SO THE CODE BELOW THAT PRINTS ON CONSOLE THE SEARCH DOES NOT EXECUTE AND BREAKS THE PROGRAM
+            error: 'You must provide a search term'
+        })
+    }
+
+    console.log(req.query.search) 
+    res.send({
+        products: []
+    })
+})
+
+app.get('/help/*', (req, res) => { // SPECIFIC ERROR IF THE ARTICLE INSIDE HELP IS NOT FOUND
+    res.render('404', {
+        title: 'Error 404',
+        errorText: 'Help article not found',
+        name: 'Lucas Aldonate'
+    })
+})
+
+app.get('*', (req, res) => { // * STANDS FOR ANYTHING THAT HASNT BEEN REFERENCED IN get() BEFORE (IT HAS TO BE AT LAST, VERY IMPORTANT)
+    res.render('404', {
+        title: 'Error 404',
+        errorText: 'Page not found',
+        name: 'Lucas Aldonate'
+    })
+})
+
+app.listen(3000, () => { // STARTS UP THE WEB SERVER
+    console.log('Server is up on port 3000.')
+})
